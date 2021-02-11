96b2949190cd5da07a7a3acdd2be77d48ab7443f
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-10 23:48:05 -0500

    Create math.h

---
+++ functionSTM/math.h
@@ -0,0 +1,9 @@
+#ifndef MATH.H
+#define MATH.H
+
+/* These are also calls as exposed function prototypes */
+int math_add(int n1, int n2);
+long long int math_mul(int n1, int n2);
+float math_dic(int n1, int n2);
+
+#endif /* MATH_H_ */
