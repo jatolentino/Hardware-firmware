1cade2069d0541fae6cb5cadb58cf601fad6cb01
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-10 23:46:40 -0500

    Create main.c

---
+++ functionSTM/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "math.h"
+int main(void)
+{
+	printf("Add: %X\n",math_add(0x0FFF1111,0x0FFF1111));
+	printf("Mul: %I64x\n",math_mul(0x0FFF1111,0x0FFF1111));
+	printf("Div: %0.2f\n",math_div(100,8));
+}
