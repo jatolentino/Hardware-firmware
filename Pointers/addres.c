8dfdfcaad7b3239e50234b6d6c45f6c497707912
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-27 18:00:59 -0500

    Create addres.c

---
+++ Pointers/addres.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+
+	long long int dat = 0xa0b3690605401601;
+	short* pAdd = (short*)&dat;// 0x0000000000401601;
+
+	printf("\n address is %p", pAdd);
+	*pAdd = 255;
+	printf("\n address is %p and value %x", pAdd, *pAdd);
+
+}
+
