c9e24ccc6b16a722a9344dcc5dae5bef31acf08c
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-27 18:01:37 -0500

    Update addres.c

--- Pointers/addres.c
+++ Pointers/addres.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
-int main(void){
-
+int main(void)
+{
 	long long int dat = 0xa0b3690605401601;
 	short* pAdd = (short*)&dat;// 0x0000000000401601;
 
 	printf("\n address is %p", pAdd);
 	*pAdd = 255;
+	printf("\n address is %p and value %x", pAdd, *pAdd);
+	
+	pAdd = pAdd + 1;
+
 	printf("\n address is %p and value %x", pAdd, *pAdd);
 
 }
