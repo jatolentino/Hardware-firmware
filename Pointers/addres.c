63a9686eafb7d4256c77f085c6b44a1f5aa2ef10
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-27 18:07:17 -0500

    Update addres.c

--- Pointers/addres.c
+++ Pointers/addres.c
@@ -2,20 +2,20 @@
 
 int main(void)
 {
-	long long int dat = 0xa0b3690605401601;
-	short* pAdd = (short*)&dat;// 0x0000000000401601;
+	long long int dat = 0xa0b3690605401601; 		// input hex data
+	short* pAdd = (short*)&dat;				// assign 2 bits of storage per address
 
-	printf("\n address is %p", pAdd);
-	*pAdd = 255;
+	printf("\n address is %p", pAdd);			// print the 1st pointer address
+	*pAdd = 255;						// modify 1st data address "01" with 255dec or ffhex
 	printf("\n address is %p and value %x", pAdd, *pAdd);
 	
-	pAdd = pAdd + 1;
+	pAdd = pAdd + 1;					// switch in 1 byte to the next address
 
-	printf("\n address is %p and value %x", pAdd, *pAdd);
+	printf("\n address is %p and value %x", pAdd, *pAdd);	// print the next address and its value which hasn't changed
 	
-	pAdd = pAdd + 1;
+	pAdd = pAdd + 1;					// switch in 1 byte to the next address again
 
-	printf("\n address is %p and value %x", pAdd, *pAdd);
+	printf("\n address is %p and value %x", pAdd, *pAdd);   // once more, print the next address and its value which hasn't changed
 
 	return 0;
 }
