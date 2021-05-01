39e58d926e28c717bd5a0a7e600604bdcce41f26
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-30 23:56:58 -0500

    Update led_on.c

--- Pointers/led_on.c
+++ Pointers/led_on.c
@@ -7,3 +7,10 @@ int main(void)
 	uint32_t *pPortD = (uint32_t*)0x40020C00;
 	uint32_t *pPortDout = (uint32_t*)0x40020C14;
 
+	// 1. Turning the clock on
+	*pClk |= 0x08;
+	*pPortD &= 0xFCFFFFFF;
+	*pPortD |= 0x01000000;
+	*pPortDout |= 0x1000;
+	
+}
