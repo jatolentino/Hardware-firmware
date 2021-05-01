9d2eb3a5c105c0f067a8043b535aa05116cb4510
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-05-01 00:00:23 -0500

    Update led_on.c

--- Pointers/led_on.c
+++ Pointers/led_on.c
@@ -4,8 +4,8 @@ int main(void)
 {
     /* Loop forever */
 	uint32_t *pClk = (uint32_t*)0x40023830;  // clock register
-	uint32_t *pPortD = (uint32_t*)0x40020C00;
-	uint32_t *pPortDout = (uint32_t*)0x40020C14;
+	uint32_t *pPortD = (uint32_t*)0x40020C00; // port moude register
+	uint32_t *pPortDout = (uint32_t*)0x40020C14; // port to output
 
 	// 1. Turning the clock on bus HBN1
 	*pClk |= 0x08;
