bc4463c278100729351f6f8f971b419d374b0c06
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-30 23:59:21 -0500

    Update led_on.c

--- Pointers/led_on.c
+++ Pointers/led_on.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     /* Loop forever */
-	uint32_t *pClk = (uint32_t*)0x40023830;
+	uint32_t *pClk = (uint32_t*)0x40023830;  // clock register
 	uint32_t *pPortD = (uint32_t*)0x40020C00;
 	uint32_t *pPortDout = (uint32_t*)0x40020C14;
 
