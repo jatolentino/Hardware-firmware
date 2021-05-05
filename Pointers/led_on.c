6948d35b4b04e8bf59922adcae2488966db3ca09
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-05-04 23:42:33 -0500

    Update led_on.c

--- Pointers/led_on.c
+++ Pointers/led_on.c
@@ -8,11 +8,11 @@ int main(void)
 	uint32_t *pPortDout = (uint32_t*)0x40020C14; // port to output
 
 	// 1. Turning the clock on bus HBN1
-	*pClk |= 0x08; // bitwise flushleft operator, no need to user masked value, pClk | (1<<4)
+	*pClk |= 0x08; // bitwise flushleft operator, no need to user masked value, pClk |= (1<<3)
 	
 	// 2. Setting the mode port the GPIOD
-	*pPortD &= 0xFCFFFFFF;
-	*pPortD |= 0x01000000;
+	*pPortD &= 0xFCFFFFFF; // pPoerD &= ~(3<<24
+	*pPortD |= 0x01000000; // pPoerD &= ~(3<<24
 	
 	// 3. Configure to ouput mode
 	*pPortDout |= 0x1000;
