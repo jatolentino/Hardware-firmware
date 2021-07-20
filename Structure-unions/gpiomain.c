4c03a0f65fc962145ad993eeb454d31ea654232b
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-07-19 23:54:33 -0500

    Update gpiomain.c

--- Structure-unions/gpiomain.c
+++ Structure-unions/gpiomain.c
@@ -5,5 +5,10 @@ int main(void)
 	RCC_AHB1NR_t volatile *const pClk = (*RCC_AHB1NR_t) 0x40023830;
 	GPIOx_MODE_t volatile *const pPortD = (*GPIOx_MODE_t) 0x40020C00;
 	GPIOx_MODE_t volatile *const pPortDout = (*GPIOx_MODE_t) 0x40020C14;
-
+	
+		
+	pClk->gpiod_en =1;
+	pPortD->pin_12 = 1;
+	pPortDout->pin12 = 1;  //set pin12 to 1
+	
 }
