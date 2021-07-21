c6436d47043b67e65b57369101092694562894d5
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-07-20 23:52:01 -0500

    Update bitfields.c

--- Structure-unions/bitfields.c
+++ Structure-unions/bitfields.c
@@ -31,3 +31,12 @@ void displayElement(struct DataSet *pdata)
 	printf("data1 = %X\n",pdata->data1);
 	printf("data2 = %X\n",pdata->data2);
 }
+	Data.crc = packetValue & 0x3;
+	Data.status = (packetValue >> 2) & 0x1;
+	Data.payload = (packetValue >> 3) & 0xFFF;
+	Data.bat = (packetValue >> 15) & 0x7;
+	Data.sensor = (packetValue >> 18) & 0x7;
+	Data.longAddr = (packetValue >> 21) & 0xFF;
+	Data.shortaddr = (packetValue >> 29) & 0x3;
+	Data.addMode = (packetValue >> 31) & 0x1;
+	
