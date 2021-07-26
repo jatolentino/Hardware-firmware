954a448f308e21636c816536bba033959fe4688c
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-07-25 23:40:29 -0500

    Update looping.c

--- Basic examples/looping.c
+++ Basic examples/looping.c
@@ -12,3 +12,17 @@ int main(void)
 	}
 	
 	
+	printf("Even numbers are :\n";
+		even = 0;
+
+		while(start_num <= end_num){
+			if(!(start_num % 2) ){
+				printf("4%d\t",start_num); //make #s take 4 places
+				even++;
+			}
+			start_num++
+		}
+		printf("\ntotal even numbers: %u\n",even);
+		wait_input();
+}
+
