9a1c3f23040083ed28dc66c8d86addbec6424ae2
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-07-25 23:40:10 -0500

    Update looping.c

--- Basic examples/looping.c
+++ Basic examples/looping.c
@@ -2,3 +2,13 @@
 #include <stdint.h>
 void wait_input(void);
 int main(void)
+{
+	int32_t start_num, end_num, even;
+	printf("Enter starting and ending memebers(give space between 2 nos):");
+	scanf("%d %d", &start_num,&end_num)
+	
+	if(end_num < start_num){
+		printf("ending number should be > starting number\n");
+	}
+	
+	
