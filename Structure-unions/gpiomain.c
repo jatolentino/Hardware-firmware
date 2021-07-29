6ce69377e7fee0e9576bef17848e864141e9ba7b
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-07-28 23:54:51 -0500

    Update gpiomain.c

--- Structure-unions/gpiomain.c
+++ Structure-unions/gpiomain.c
@@ -6,7 +6,6 @@ int main(void)
 	GPIOx_MODE_t volatile *const pPortD = (*GPIOx_MODE_t) 0x40020C00;
 	GPIOx_MODE_t volatile *const pPortDout = (*GPIOx_MODE_t) 0x40020C14;
 	
-		
 	pClk->gpiod_en =1;
 	pPortD->pin_12 = 1;
 	pPortDout->pin12 = 1;  //set pin12 to 1 (led turns on)
