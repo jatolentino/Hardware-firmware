558580693aaed30ba1b92e54b4aae4cfcbc761d1
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-08-02 23:49:51 -0500

    Update main.c

--- Keypad/main.c
+++ Keypad/main.c
@@ -6,5 +6,7 @@ int main(main)
   //Adding the peripherla registers addresses
   uint32_t volatile *const pGpioModeReg = (uint32_t*)0x40020C00;
   uint32_t volatile *const pInPutDataReg = (uint32_t*)0x40020C00 + 0x10;
-  
+  uint32_t volatile *cohst pOutDataReg = (uint32_t*)0x40020C00 + 0x14;
+  uint32_t volatile *const pClockCtrlReg = (uint32_t*)0x40023800 + 0x30;
+  uint32_t volatile *const pPulupDownReg = (uint32_t*)0x40020C00 + 0x0C;
 }
