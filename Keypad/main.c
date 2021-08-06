36fcbb8d15668b05eff75329382883fca1c8bb17
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-08-06 07:30:51 -0500

    Update main.c

--- Keypad/main.c
+++ Keypad/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void delay(){
+  for(uint32_t i=0; i<300000 ; i++);
 }
 int main(main)
 {
@@ -31,90 +32,92 @@ int main(main)
   // 00      00      00        00
   *GpioModereg &= ~(0xFF<<16);
   
-  // Set rows to HIGH, binary is 1111 = 0x0F
-  *pOutDataReg |= 0x0F;
   
-  // Set 1st ROW to low
-  *pOutDataReg &= ~(1<<0); //PD0
+  while(1){
+    // Set rows to HIGH, binary is 1111 = 0x0F
+    *pOutDataReg |= 0x0F;
+
+    // Set 1st ROW to low
+    *pOutDataReg &= ~(1<<0); //PD0
+
+    // Scan the columns
+    // Check the inputs if high
+    if (!(0x40020C10 & (1 << 8))){  //PD8
+      delay(); 
+      printf("1\n");
+    }
+    if (!(0x40020C10 & (1 << 9))){  //PD9
+      delay(); 
+      printf(2"\n"); 
+    }
+    if (!(0x40020C10 & (1 << 10))){  //PD10
+      delay(); 
+      printf("3\n");
+    }
+    if (!(0x40020C10 & (1 << 11))){  //PD11
+      delay();
+      printf("A\n");
+    }
+
+    //Make the 2nd row low and all rows high
+    *pOutDataReg |= 0x0F; 
+    *pOutDataReg &= ~(1<<1);  //PD1
+    if (!(0x40020C10 & (1 << 8))){
+      delay();
+      printf("4\n");
+    }
+    if (!(0x40020C10 & (1 << 9))){
+      delay(); 
+      printf(5"\n");
+    }
+    if (!(0x40020C10 & (1 << 10))){
+      delay(); 
+      printf("6\n");
+    }
+    if (!(0x40020C10 & (1 << 11))){
+      delay(); 
+      printf("B\n");
+    }
+
+    //Make the 3rd row low and all rows high
+    *pOutDataReg |= 0x0F;   //PD2
+    *pOutDataReg &= ~(1<<2);
+    if (!(0x40020C10 & (1 << 8))){
+      delay();
+      printf("7\n");
+    }
+    if (!(0x40020C10 & (1 << 9))){
+      delay(); 
+      printf(8"\n");
+    }
+    if (!(0x40020C10 & (1 << 10))){
+      delay(); 
+      printf("9\n");
+    }
+    if (!(0x40020C10 & (1 << 11))){
+      delay(); 
+      printf("C\n");
+    }
+
+    //Make the 3rd row low and all rows high
+    *pOutDataReg |= 0x0F;
+    *pOutDataReg &= ~(1<<3)  //PD3
+    if (!(0x40020C10 & (1 << 8))){
+      delay();
+      printf("*\n");
+    }
+    if (!(0x40020C10 & (1 << 9))){
+      delay(); 
+      printf("0\n");
+    }
+    if (!(0x40020C10 & (1 << 10))){
+      delay(); 
+      printf("#\n");
+    }
+    if (!(0x40020C10 & (1 << 11))){
+      delay(); 
+      printf("D\n");
+    } 
   
-  // Scan the columns
-  // Check the inputs if high
-  if (!(0x40020C10 & (1 << 8))){  //PD8
-    delay(); 
-    printf("1\n");
   }
-  if (!(0x40020C10 & (1 << 9))){  //PD9
-    delay(); 
-    printf(2"\n"); 
-  }
-  if (!(0x40020C10 & (1 << 10))){  //PD10
-    delay(); 
-    printf("3\n");
-  }
-  if (!(0x40020C10 & (1 << 11))){  //PD11
-    delay();
-    printf("A\n");
-  }
-  
-  //Make the 2nd row low and all rows high
-  *pOutDataReg |= 0x0F; 
-  *pOutDataReg &= ~(1<<1);  //PD1
-  if (!(0x40020C10 & (1 << 8))){
-    delay();
-    printf("4\n");
-  }
-  if (!(0x40020C10 & (1 << 9))){
-    delay(); 
-    printf(5"\n");
-  }
-  if (!(0x40020C10 & (1 << 10))){
-    delay(); 
-    printf("6\n");
-  }
-  if (!(0x40020C10 & (1 << 11))){
-    delay(); 
-    printf("B\n");
-  }
-  
-  //Make the 3rd row low and all rows high
-  *pOutDataReg |= 0x0F;   //PD2
-  *pOutDataReg &= ~(1<<2);
-  if (!(0x40020C10 & (1 << 8))){
-    delay();
-    printf("7\n");
-  }
-  if (!(0x40020C10 & (1 << 9))){
-    delay(); 
-    printf(8"\n");
-  }
-  if (!(0x40020C10 & (1 << 10))){
-    delay(); 
-    printf("9\n");
-  }
-  if (!(0x40020C10 & (1 << 11))){
-    delay(); 
-    printf("C\n");
-  }
-  
-  //Make the 3rd row low and all rows high
-  *pOutDataReg |= 0x0F;
-  *pOutDataReg &= ~(1<<3)  //PD3
-  if (!(0x40020C10 & (1 << 8))){
-    delay();
-    printf("*\n");
-  }
-  if (!(0x40020C10 & (1 << 9))){
-    delay(); 
-    printf("0\n");
-  }
-  if (!(0x40020C10 & (1 << 10))){
-    delay(); 
-    printf("#\n");
-  }
-  if (!(0x40020C10 & (1 << 11))){
-    delay(); 
-    printf("D\n");
-  } 
-  
-   
 }
