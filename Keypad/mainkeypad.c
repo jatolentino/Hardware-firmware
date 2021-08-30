ef025b1e32b68fb30d3d800ba48b2c247ad263ba
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-08-29 23:56:42 -0500

    Update mainkeypad.c

--- Keypad/mainkeypad.c
+++ Keypad/mainkeypad.c
@@ -6,7 +6,7 @@ void delay(){
 }
 int main(main){
 	//Adding the peripherla registers addresses
-	uint32_t volatile *const pGpioModeReg = (uint32_t*)0x40020C00;
+	uint32_t volatile *const pGpioModeReg = (uint32_t*)0x40020C00; // choosing the PIN mode
 	uint32_t volatile *const pInPutDataReg = (uint32_t*)0x40020C00 + 0x10;
 	uint32_t volatile *const pOutDataReg = (uint32_t*)0x40020C00 + 0x14;
 	uint32_t volatile *const pClockCtrlReg = (uint32_t*)0x40023800 + 0x30;
